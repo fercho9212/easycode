d684e891db55e5a5573d2eb1dd6fe1755a6eb71e
vagrant <vagrant@homestead>
2016-12-21 22:21:07 +0000

    add migrate and foreign key to images

--- database/migrations/2016_12_21_220553_create_images_table.php
+++ database/migrations/2016_12_21_220553_create_images_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArticlesTable extends Migration
+class CreateImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');
-            $table->text('content');
-            $table->integer('category_id')->unsigned();
-            //foreign keys
-            $table->integer('user_id')->unsigned();
+            $table->string('name');
+            $table->integer('article_id')->unsigned();
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('articles');
+        Schema::dropIfExists('images');
     }
 }
