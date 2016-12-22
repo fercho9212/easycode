6e83d4e3c32e0ddf905223411ef7498fcd6c6fe2
fercho <vagrant@homestead>
2016-12-22 04:59:57 +0000

    this is a test  update welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Holaaaa</a>
                     @else
                         <a href="{{ url('/login') }}">Login</a>
                         <a href="{{ url('/register') }}">Register</a>
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Holaaa this is a test
                 </div>
 
                 <div class="links">
