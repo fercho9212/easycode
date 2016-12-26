9123d567e3ebf5d544d6fb098f09044359de077d
Fercho Jerez <vagrant@homestead>
2016-12-26 18:47:47 +0000

    Add seeder for articles , category, and users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-    
+
         $this->call('CategoryTableSeeder');
+        $this->call('UserTableSeeder');
+        $this->call('ArticleTableSeeder');
     }
 }
